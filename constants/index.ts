c6ceb6cf642bcfa0c364cb13f8cc847e181d3048
StephenAraka <stevenaraka@gmail.com>
2024-12-17 23:29:45 +0300

    Add lottie animation files

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,22 @@
 import search from "../assets/icons/search.png";
 import pin from "../assets/icons/pin.png";
+import cloud from "../assets/lottie/cloud.json";
+import rain from "../assets/lottie/rain.json";
+import suncloud from "../assets/lottie/sun-cloud.json";
+import thunderstorm from "../assets/lottie/thunderstorm.json";
+import sun from "../assets/lottie/sun.json";
+import moon from "../assets/lottie/moon.json";
 
 export const icons = {
   search,
-  pin
+  pin,
 };
+
+export const animations = {
+  rain,
+  thunderstorm,
+  suncloud,
+  cloud,
+  sun,
+  moon
+}
