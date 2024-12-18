84d36476a434971cbff017acc8edca86f94ce456
StephenAraka <stevenaraka@gmail.com>
2024-12-18 11:50:18 +0300

    Define weather interfaces

--- types/types.d.ts
+++ types/types.d.ts
@@ -6,4 +6,69 @@ declare interface InputFieldProps extends TextInputProps {
   inputStyle?: string;
   iconStyle?: string;
   className?: string;
+  loading?: boolean;
+}
+
+declare interface Location {
+  city: string,
+  country: string,
+  countryCode: string,
+  id: number,
+  latitude: number,
+  longitude: number,
+  name: string,
+  population: number,
+  region: string,
+  regionCode: string,
+  regionWdId: string,
+  type: string,
+  wikiDataId: string
+}
+
+declare interface WeatherData {
+  base: string;
+  city: string;
+  clouds: {
+    all: number;
+  };
+  cod: number;
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  dt: number;
+  id: number;
+  main: {
+    feels_like: number;
+    grnd_level: number;
+    humidity: number;
+    pressure: number;
+    sea_level: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  name: string;
+  sys: {
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  visibility: number;
+  weather: Array<{
+    description: string;
+    icon: string;
+    id: number;
+    main: string;
+  }>;
+  wind: {
+    deg: number;
+    gust: number;
+    speed: number;
+  };
+}
+
+declare interface WeatherDataProps {
+  data: WeatherData;
 }
