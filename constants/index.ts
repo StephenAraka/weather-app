963d55f679de097706dc93573281dcc82c1c7b80
StephenAraka <stevenaraka@gmail.com>
2024-12-18 10:24:34 +0300

    Export standard weather icons

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,5 @@
-import search from "../assets/icons/search.png";
-import pin from "../assets/icons/pin.png";
+// import search from "../assets/icons/search.png";
+// import pin from "../assets/icons/pin.png";
 import cloud from "../assets/lottie/cloud.json";
 import rain from "../assets/lottie/rain.json";
 import suncloud from "../assets/lottie/sun-cloud.json";
@@ -7,9 +7,29 @@ import thunderstorm from "../assets/lottie/thunderstorm.json";
 import sun from "../assets/lottie/sun.json";
 import moon from "../assets/lottie/moon.json";
 
+// import icon01d from "../assets/icons/01d.png";
+// import icon01n from "../assets/icons/01n.png";
+// import icon02d from "../assets/icons/02d.png";
+// import icon02n from "../assets/icons/02n.png";
+// import icon03d from "../assets/icons/03d.png";
+// import icon03n from "../assets/icons/03n.png";
+// import icon04d from "../assets/icons/04d.png";
+// import icon04n from "../assets/icons/04n.png";
+// import icon09d from "../assets/icons/09d.png";
+// import icon09n from "../assets/icons/09n.png";
+// import icon10d from "../assets/icons/10d.png";
+// import icon10n from "../assets/icons/10n.png";
+// import icon11d from "../assets/icons/11d.png";
+// import icon11n from "../assets/icons/11n.png";
+// import icon13d from "../assets/icons/13d.png";
+// import icon13n from "../assets/icons/13n.png";
+// import icon50d from "../assets/icons/50d.png";
+// import icon50n from "../assets/icons/50n.png";
+// import unknown from "../assets/icons/unknown.png";
+
 export const icons = {
-  search,
-  pin,
+  search: require("../assets/icons/search.png"),
+  pin: require("../assets/icons/pin.png"),
 };
 
 export const animations = {
@@ -19,4 +39,26 @@ export const animations = {
   cloud,
   sun,
   moon
+}
+
+export const weatherIcons = {
+  icon01d: require("../assets/icons/01d.png"),
+  icon01n: require("../assets/icons/01n.png"),
+  icon02d: require("../assets/icons/02d.png"),
+  icon02n: require("../assets/icons/02n.png"),
+  icon03d: require("../assets/icons/03d.png"),
+  icon03n: require("../assets/icons/03n.png"),
+  icon04d: require("../assets/icons/04d.png"),
+  icon04n: require("../assets/icons/04n.png"),
+  icon09d: require("../assets/icons/09d.png"),
+  icon09n: require("../assets/icons/09n.png"),
+  icon10d: require("../assets/icons/10d.png"),
+  icon10n: require("../assets/icons/10n.png"),
+  icon11d: require("../assets/icons/11d.png"),
+  icon11n: require("../assets/icons/11n.png"),
+  icon13d: require("../assets/icons/13d.png"),
+  icon13n: require("../assets/icons/13n.png"),
+  icon50d: require("../assets/icons/50d.png"),
+  icon50n: require("../assets/icons/50n.png"),
+  unknown: require("../assets/icons/unknown.png"),
 }
