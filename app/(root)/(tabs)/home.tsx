e34d69af62e2df01b5f4ccb251aa50f625dd271c
StephenAraka <stevenaraka@gmail.com>
2024-12-18 15:36:48 +0300

    Add welcome message to homepage

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,15 +1,14 @@
 import CurrentWeatherCard from '@/components/CurrentWeather';
 import WeatherForecast from '@/components/WeatherForecast';
 import { useState } from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useQuery } from '@tanstack/react-query';
-import {
-  fetchCities,
-  searchWeatherData,
-} from '@/lib/api';
+import { fetchCities, searchWeatherData } from '@/lib/api';
 import { Location } from '../../../types/types';
 import CitySearch from '@/components/CitySearch';
+import LottieView from 'lottie-react-native';
+import { animations } from '@/constants';
 
 const Home = () => {
   const [cityName, setCityName] = useState('');
@@ -18,8 +17,6 @@ const Home = () => {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
 
-
-
   const handleLocation = async (selected: Location) => {
     setShowLocations(false);
     const { city, countryCode, latitude, longitude } = selected;
@@ -27,8 +24,12 @@ const Home = () => {
 
     try {
       // Await the data returned by the async function
-      const { currentWeather, forecast } = await searchWeatherData({ city: label, latitude, longitude });
-  
+      const { currentWeather, forecast } = await searchWeatherData({
+        city: label,
+        latitude,
+        longitude,
+      });
+
       // Update state with the fetched data
       setCurrentWeather({ city, ...currentWeather });
       setForecast({ city, ...forecast });
@@ -75,6 +76,32 @@ const Home = () => {
         {forecast && (
           <View className="mt-4 mx-4">
             <WeatherForecast data={forecast} />
+          </View>
+        )}
+
+        {/*//- EMPTY STATE  */}
+        {!forecast && !currentWeather && (
+          <View className="flex-1 justify-center items-center p-4">
+            <LottieView
+              loop
+              source={animations.sun}
+              autoPlay
+              style={{
+                height: 360,
+                marginTop: 20,
+                width: 360,
+              }}
+            />
+            <Text className="text-2xl font-bold text-blue-800 mb-2">
+              🌤️ Welcome to AnyWeather!
+            </Text>
+            <Text className="text-lg text-gray-700 text-center mb-4">
+              Find out the current weather and few days forecast for any city in
+              the world. Start by searching for a city above.
+            </Text>
+            <Text className="text-sm text-gray-500 mt-4 text-center">
+              Your perfect travel companion for staying ahead of the weather!
+            </Text>
           </View>
         )}
       </ScrollView>
